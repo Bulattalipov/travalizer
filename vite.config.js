bdf3991efa2cd417eef5a2f10b042d64e525a383
Bulat <bulat0talipov@gmail.com>
2024-07-04 17:02:13 +0300

    added new file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ?
-    '/my/dev/baseurl/' : '/my/prod/baseurl/',
   plugins: [vue()],
 })
